db64d1bde95393d9e075631ea985f61129f39404
Helena <inger.helena@gmail.com>
2020-09-07 14:54:50 +0100

    created EmployeeRepository interface

--- src/test/java/com/codeclan/example/employeeservice/EmployeeserviceApplicationTests.java
+++ src/test/java/com/codeclan/example/employeeservice/EmployeeserviceApplicationTests.java
@@ -1,13 +1,25 @@
 package com.codeclan.example.employeeservice;
 
+import com.codeclan.example.employeeservice.models.Employee;
+import com.codeclan.example.employeeservice.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmployeeserviceApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void createPirate() {
+		Employee jack = new Employee("Jack", 30, "jack@gmail.com");
+		employeeRepository.save(jack);
 	}
 
 }
